ccfa3385c47499ba70391176899dd2cc30f3f2e2
Charlie Thomas <charlie.thomas321@gmail.com>
2017-02-25 20:25:15 +0000

    Added all_students view

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# add @login_required
+def all_students(request):
+    response = render(request, 'nucleus/students.html', context={})
+    return response
+
+
