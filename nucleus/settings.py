7f2001a36b204167f5d15050f39825c4501c8042
Charlie Thomas <charlie.thomas321@gmail.com>
2017-02-25 20:53:45 +0000

    Updated urls and added static folder for css

--- nucleus/settings.py
+++ nucleus/settings.py
@@ -18,9 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -62,6 +59,7 @@ PASSWORD_HASHERS = (
 
 ROOT_URLCONF = 'nucleus.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- nucleus/urls.py
+++ nucleus/urls.py
@@ -24,9 +24,9 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.sign_in, name='index'),
     url(r'^students/', views.all_students, name="all_students"),
-	url(r'^sign-in/',views.sign_in, name="sign_in"),
-	url(r'^register/',views.register, name="register"),
-	url(r'^forgot-password',views.forgot_password, name="forgot_password"),
-	url(r'^logout/$', views.user_logout, name='user_logout'),
+    url(r'^sign-in/',views.sign_in, name="sign_in"),
+    url(r'^register/',views.register, name="register"),
+    url(r'^forgot-password',views.forgot_password, name="forgot_password"),
+    url(r'^logout/$', views.user_logout, name='user_logout'),
     url(r'^admin/', admin.site.urls),
 ]
