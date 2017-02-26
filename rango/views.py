e565b995c9869a96da89c6575c6268232dbf915a
Charlie Thomas <charlie.thomas321@gmail.com>
2017-02-26 13:26:29 +0000

    Added Student model and updated the all_students view context

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from rango.models import Student
 
 
 # add @login_required
 def all_students(request):
-    response = render(request, 'nucleus/students.html', context={})
-    return response
 
+    student_list = Student.objects.order_by('guid')
 
+    context_dict = { 'students': student_list }
+
+    return render(request, 'nucleus/students.html', context=context_dict)
