17a3c340e0627c2629b00f5a0dfd101c2b5b9616
Charlie Thomas <charlie.thomas321@gmail.com>
2017-02-26 16:04:34 +0000

    Added more dummy data to all_students view

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,13 @@ def all_students(request):
 
     #student_list = Student.objects.order_by('guid')
 
-    context_dict = { 'students': ['2198970T', '2198989X'] }
+    context_dict = { 'students': [
+                        {'guid': '2198970T', 'name': 'Charlie Thomas', 'score': 69},
+                        {'guid': '2198989S', 'name': 'John Smith', 'score': 34},
+                        {'guid': '2174939D', 'name': 'Jared Dunn', 'score': 50},
+                        {'guid': '2598741W', 'name': 'Willy Wonka', 'score': 69},
+                        {'guid': '2174057L', 'name': 'James Lorne', 'score': 11},
+                        {'guid': '2134567R', 'name': 'Ewan Redface', 'score': 43}]
+                    }
 
     return render(request, 'nucleus/students.html', context=context_dict)
