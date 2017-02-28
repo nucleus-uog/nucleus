422bb751048758065633550276177990ff16b071
David <david@davidtw.co>
2017-02-28 15:41:51 +0000

    Added repository url field.

--- rango/models.py
+++ rango/models.py
@@ -11,10 +11,11 @@ from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=False)
+    last_name = models.CharField(_('last name'), max_length=30, blank=False)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
+    repository_url = models.CharField(_('repository url'), max_length=60, blank=False)
 
     objects = UserManager()
 
