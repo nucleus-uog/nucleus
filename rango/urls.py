b6acc83d4ffe6930c77feb71a28f6123a58f0a9f
Charlie Thomas <charlie.thomas321@gmail.com>
2017-03-19 13:51:15 +0000

    Ammeded student page url

--- rango/urls.py
+++ rango/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^students/$', views.all_students, name="all_students"),
-    url(r'^student/(?P<student_guid>[\w\-]+)/$', views.student, name='student'),
+    url(r'^student/(?P<student_guid>\d{7}[A-Za-z])/$', views.student, name='student'),
     url(r'^demo/$', views.demo, name='demo'),
     url(r'^demo/run/$', views.demo_run, name='demo_run'),
     url(r'^register$', views.register, name="register"),
