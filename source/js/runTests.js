2f55222acb134cb8a99853a6afb66d4c14551ec0
Charlie Thomas <charlie.thomas321@gmail.com>
2017-03-19 16:36:08 +0000

    Ajax implemented to refresh test status automatically

--- source/js/runTests.js
+++ source/js/runTests.js
@@ -1,10 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var buttonElement = document.getElementById('runTests');
-    buttonElement.onclick = function() {
-        var req = new XMLHttpRequest();
-        req.open('GET', window.location.protocol + '//' + window.location.host + '/demo/run');
-        req.send(null);
-        buttonElement.disabled = true;
-    };
-    buttonElement.disabled = false;
+    var intervalId;
+    updateStatus = function() {
+        var elements = document.getElementsByClassName('status-check');
+        if(elements.length < 1) clearInterval(intervalId);
+        for (var i = 0; i < elements.length; i++) {
+            var el = elements[i];
+            var id = el.id;
+            id = id.replace('status-', '');
+
+            var req = new XMLHttpRequest();
+            req.open('GET', window.location.protocol + '//' + window.location.host + '/status/'+id);
+            req.send(null);
+            req.onreadystatechange = changeStatus;
+        }
+    }
+    updateStatus();
+    intervalId = setInterval(updateStatus, 5000);
 });
+
+function changeStatus(){
+  if(this.readyState == 4 && this.status == 200){
+    var data = JSON.parse(this.responseText);
+    var el = document.getElementById('status-'+data['id']);
+    el.className = data['class'];
+    el.innerHTML = data['status'];
+  }
+}
