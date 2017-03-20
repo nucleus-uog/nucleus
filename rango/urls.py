e047a878a77e4c886c2687bbb3e853ff9fa4bdd6
David Wood <david@davidtw.co>
2017-03-20 11:26:19 +0000

    Fixed errors from the great rebasing.

--- rango/urls.py
+++ rango/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^students/$', views.all_students, name="all_students"),
     url(r'^student/(?P<student_guid>\d{7}[A-Za-z])/$', views.student, name='student'),
-    url(r'^status/(?P<runid>\d+)/$', views.check_status, name='check_status')
+    url(r'^status/(?P<runid>\d+)/$', views.check_status, name='check_status'),
     url(r'^demo/$', views.demo, name='demo'),
     url(r'^demo/run/$', views.demo_run, name='demo_run'),
-    url(r'^run_all/$', views.run_all, name="run_all")
+    url(r'^run_all/$', views.run_all, name="run_all"),
     url(r'^register$', views.register, name="register"),
     url(r'^account$', views.account, name="account"),
 ]
