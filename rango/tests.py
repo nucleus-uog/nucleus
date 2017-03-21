29d4c01bcce1c377e31ef44651c3cf27b8104833
John Devine <john.devine1776@gmail.com>
2017-03-21 14:11:19 +0000

    Added unit test that checks that an invalid user can't login.

--- rango/tests.py
+++ rango/tests.py
@@ -4,22 +4,6 @@ from .models import *
 from .forms import *
 
 
-class SetupClass(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create(
-            first_name='John',
-            last_name='Dev',
-            email='2162978D@student.gla.ac.uk',
-        )
-        self.user.set_password('Blueisthecolour')
-        self.user.save()
-
-
-
-
-
-
 class RegisterUserFormTest(TestCase):
     '''Checks that data must be entered into the form fields '''
     def test_UserForm_valid(self):
@@ -44,24 +28,34 @@ class RegisterUserFormTest(TestCase):
 
 class LoginViewSuccessTest(TestCase):
 
-    '''Checks that a valid user can login'''
-    def test_login_credentials_valid(self):
 
+    def setUp(self):
         self.user = User.objects.create(
             first_name='John',
             last_name='Dev',
             email='2162978D@student.gla.ac.uk',
         )
-
         self.user.set_password('Blueisthecolour')
         self.user.save()
-        user = User.objects.get(email='2162978D@student.gla.ac.uk')
+
+    '''Checks that a valid user can login'''
+    def test_login_credentials_valid(self):
         user_login = self.client.login(email='2162978D@student.gla.ac.uk', password='Blueisthecolour')
         self.assertTrue(user_login)
         response = self.client.get(reverse('student', kwargs={'student_guid':'2162978D'}))
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
-    #Check that invalid user can't login 
+
+
+    '''Check that invalid user can't login'''
+    def test_login_credentials_invalid(self):
+        response = self.client.post(reverse('sign_in'), {'username': 'wronguser', 'password': 'wrongpass'})
+
+        # The sign is page should be reloaded.
+        self.assertEqual(response.status_code, 200)
+        # Check that error message is displayed.
+        self.assertIn("Your username and password didn't match. Please try again.", response.content)
+
 
 
 
