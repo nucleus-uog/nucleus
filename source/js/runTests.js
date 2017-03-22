194b7f12f83d830fafdc184a55607afcedb3e4e5
John Devine <john.devine1776@gmail.com>
2017-03-22 12:41:53 +0000

    Automatically refresh page on test completion

--- source/js/runTests.js
+++ source/js/runTests.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var intervalId;
     updateStatus = function() {
+        // Gets the badge of tests currently running
         var elements = document.getElementsByClassName('status-check');
         if(elements.length < 1) clearInterval(intervalId);
+
+        // Updates the status of the tests automatically 
         for (var i = 0; i < elements.length; i++) {
             var el = elements[i];
             var id = el.id;
@@ -23,6 +26,12 @@ function changeStatus(){
     var data = JSON.parse(this.responseText);
     var el = document.getElementById('status-'+data['id']);
     el.className = data['class'];
-    el.innerHTML = '<i class="' + data['icon'].replace("\"", '') + '"></i> ' + data['status'];
+
+    // Updates status badge if the status of the tests has changed
+    var inner = '<i class="' + data['icon'].replace("\"", '') + '"></i> ' + data['status'];
+    if(el.innerHTML != inner) el.innerHTML = inner;
+
+    // Reloads the page if a test has complete
+    if(!data['class'].includes("status-check")) location.reload();
   }
 }
